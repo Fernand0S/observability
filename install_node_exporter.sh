9cef0cc4441ba87d3e5deabf7bac8479cef6187c
Fernando Santana <32446123+Fernand0S@users.noreply.github.com>
2023-08-14 12:44:38 -0300

    Update install_node_exporter.sh

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -1,17 +1,14 @@
 #Adiciona usuário 
 useradd -M -r -s /bin/false node_exporter
 
-#Download Node Exporter
-wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
-
-#Descompactando pacote Node Exporter
-tar xzf node_exporter-0.18.1.linux-amd64.tar.gz
+#Download Node Exporter e Descompactando pacote Node Exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz && tar xvzf node_exporter-1.6.1.linux-amd64.tar.gz
 
 #Copia arquivo para o diretório
-cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
+mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
 
-#Copia arquivo de servico Node Exporter
-cp /node_exporter.service /etc/systemd/system/
+#Removendo node_exporter
+rm -rf node_exporter-1.6.1.linux-amd64*
 
 #Alterar as permissões do diretório Node Exporter
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
@@ -30,6 +27,9 @@ Type=simple
 ExecStart=/usr/local/bin/node_exporter --collector.cpu --web.max-requests=10 --web.disable-exporter-metrics --collector.diskstats --collector.meminfo --collector.netstat --collector.qdisc --collector.cpufreq --collector.processes --collector.tcpstat
 [Install]
 WantedBy=multi-user.target"
+
+#Copia arquivo de servico Node Exporter
+cp /node_exporter.service /etc/systemd/system/
 
 
 #Habilita o serviço do Node Exporter
